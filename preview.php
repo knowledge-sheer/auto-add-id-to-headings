8e2c7d486d8ee5201158c9afca6ed9b30f3d1050
ashwin-pandey <ashwin.pandey11197@gmail.com>
2019-09-12 18:05:03 +0530

    add toc

--- preview.php
+++ preview.php
@@ -17,8 +17,16 @@ if (isset($_POST['submit'])) {
 
 	<div class="container">
 		<div class="row">
-			<div class="col-md-7 mx-auto">
+			<div class="col-md-7 mx-auto" id="toc-container">
 				<?php echo $content; ?>
+			</div>
+			<div class="col-md-3">
+				<div class="card card-small">
+					<div class="card-header">Table Of Contents</div>
+					<div class="card-body p-2">
+						<ol class="list-group" id="toc-main"></ol>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -26,5 +34,21 @@ if (isset($_POST['submit'])) {
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<script>
+
+$(document).ready(function toc() {
+	var children = $("#toc-container").children(".toc-item");
+
+	for (var i = 0; i < children.length; i++) {
+		$(children[i]).prop("id", "toc-item-"+i);
+		html += "<li class='list-group-item'><a href='#toc-item-"+ i +"'>" + $(children[i]).html() + "</a></li>";
+	}
+
+	$("#toc-main").html(html);
+});
+
+</script>
+
 </body>
 </html>
